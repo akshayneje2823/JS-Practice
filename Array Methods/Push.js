ab2b251a3b74427d97f88d6a74799f38bd07a3cf
Akshay Neje <akshayneje2823@gmail.com>
2022-04-17 14:17:47 +0530

    Done

---
+++ Array Methods/Push.js
@@ -0,0 +1,5 @@
+// 
+
+const Arr = ["A","B","C","D","E","F"]
+Arr2 = Arr.push()
+console.log(Arr2)
---
+++ Array Methods/Slice.js
@@ -0,0 +1,6 @@
+// SLICE ==> RETURNS NEW ARRAY AND CONATIN DELETED ITEM
+
+var a = [11,22,33,44,55,66,77,88,99]
+// console.log(a.slice(0,3))
+// console.log(a.slice())
+console.log(a.slice(5))
