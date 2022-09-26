dd1d2a34563ffb7f3d5d72509f82ffa2e7939cd1
Akshay Neje <akshayneje2823@gmail.com>
2022-09-26 22:14:19 +0530

    Done

--- Pure Function/pure.js
+++ Pure Function/pure.js
@@ -1,26 +1,43 @@
-function add(x){
-    return ++x         //modyfing arguments
-};
-console.log(add(5))
+// function add(x){
+//     return ++x         //modyfing arguments
+// };
+// console.log(add(5))
 
 
-// Pure Function
-function add2(x){
-    return x=1        //doesn't modify argumnets
-};
-console.log(add2(5))
-
-
-
+// // Pure Function
+// function add2(x){
+//     return x=1        //doesn't modify argumnets
+// };
+// console.log(add2(5))
 
 
 
 
 
 
+// const array = [1,2,3,4,4,4,2];
+
+// const findDup = array => array.filter((item,index)=> array.indexOf(item) !== index);
+
+// const dupliEle = findDup(array)
+
+// console.log(dupliEle)
+let max = 5
+let n = 1
+
+for (let i = 1; i < max; i++) {
+    for (let j = 1; j < n; j++) {
+        console.log(i)
+    }
+
+}
+// function values(a, b) {
+   
+//     return 
 
 
-
+// // }
+// console.log(values(max, n))
 
 
 
